9af28bde1f7434afbea14e560c559e22527ad4f9
InkoHX <inkohx@gmail.com>
2019-01-14 03:09:55 +0900

    Update

--- src/VectorNetworkProject/Framework/command/FrameworkCommand.php
+++ src/VectorNetworkProject/Framework/command/FrameworkCommand.php
@@ -23,12 +23,14 @@ abstract class FrameworkCommand extends Command
      * FrameworkCommand constructor.
      *
      * @param PluginBase $plugin
-     * @param string $name
+     * @param string $name コマンドの名前
+     * @param bool $isAdmin コマンドの権限設定
      */
-    public function __construct(PluginBase $plugin, string $name)
+    public function __construct(PluginBase $plugin, string $name, $isAdmin = false)
     {
-        parent::__construct($name);
         $this->plugin = $plugin;
+        $this->PermissionManager()->add($name, $this->description, $isAdmin);
+        parent::__construct($name);
     }
 
     /**
@@ -72,5 +74,13 @@ abstract class FrameworkCommand extends Command
     public function getPlugin(): PluginBase
     {
         return $this->plugin;
+    }
+
+    /**
+     * @return FrameworkPermission
+     */
+    public function PermissionManager(): FrameworkPermission
+    {
+        return new FrameworkPermission($this->plugin->getName());
     }
 }
