f4f9b0b9c00a906ad30d5cd5f63d11850e9def06
InkoHX <inkohx@gmail.com>
2019-01-16 19:22:40 +0900

    Add PlayerGuild

---
+++ src/VectorNetworkProject/Framework/player/guild/PlayerGuild.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Copyright (c) 2019 VectorNetworkProject. All rights reserved. MIT license.
+ *
+ * GitHub: https://github.com/VectorNetworkProject/Framework
+ * Website: https://www.vector-network.tk
+ */
+
+namespace VectorNetworkProject\Framework\player\guild;
+
+
+class PlayerGuild
+{
+
+}
