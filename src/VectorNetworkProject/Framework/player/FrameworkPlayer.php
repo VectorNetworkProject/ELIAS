997de08753429d4e910599fdea0f884826156b46
InkoHX <inkohx@gmail.com>
2019-01-16 19:22:04 +0900

    Update FrameworkPlayer

---
+++ src/VectorNetworkProject/Framework/player/FrameworkPlayer.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Copyright (c) 2019 VectorNetworkProject. All rights reserved. MIT license.
+ *
+ * GitHub: https://github.com/VectorNetworkProject/Framework
+ * Website: https://www.vector-network.tk
+ */
+
+namespace VectorNetworkProject\Framework\player;
+
+use pocketmine\Player;
+
+class FrameworkPlayer
+{
+    /** @var Player $player */
+    private $player;
+    
+    public function __construct(Player $player)
+    {
+        $this->player = $player;
+    }
+
+    public function getGuild()
+    {
+        // TODO:
+    }
+
+    public function getLanguage()
+    {
+        // TODO:
+    }
+    
+    /**
+     * @return Player
+     */
+    public function getPlayer(): Player
+    {
+        return $this->player;
+    }
+}
