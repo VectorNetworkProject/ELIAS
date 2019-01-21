a7c28d85bbbb0794c8e41cbe9f248893770cc979
InkoHX <inkohx@gmail.com>
2019-01-21 19:11:05 +0900

    Update Contants

--- src/VectorNetworkProject/Framework/util/Constants.php
+++ src/VectorNetworkProject/Framework/util/Constants.php
@@ -10,16 +10,58 @@ namespace VectorNetworkProject\Framework\util;
 
 class Constants
 {
+    /** @var array $option */
     public static $option = [
-        'framework' => true,
+        'framework' => [
+            'enabled' => true,
+            'discord' => [
+                'webhook' => [
+                    'error-report' => [
+                        'enabled' => false,
+                        'url' => null
+                    ],
+                    'crash-report' => [
+                        'enabled' => false,
+                        'url' => null
+                    ]
+                ]
+            ]
+        ],
+        'provider' => [
+            'user' => []
+        ]
     ];
 
     /**
-     * @return array
+     * @param string $key
+     * @return mixed
+     *
+     * @example Constants::getOption('provider.user'); // array()
      */
-    public static function getOption(): array
+    public static function getOption(string $key)
     {
-        return self::$option;
+        if (isset(self::$option[$key])) {
+            return self::$option[$key];
+        }
+
+        $vars = explode(".", $key);
+        $keys = array_shift($vars);
+        if (isset(self::$option[$keys])) {
+            $keys = self::$option[$keys];
+        } else {
+            return null;
+        }
+
+        while (count($vars) > 0) {
+            $baseKey = array_shift($vars);
+            if (is_array($keys) && isset($keys[$baseKey])) {
+                $keys = $keys[$baseKey];
+            } else {
+                return null;
+            }
+        }
+
+        return self::$option[$key] = $keys;
     }
 
     /**
