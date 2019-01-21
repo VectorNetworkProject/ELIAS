2d832078470d3d56734200061ca9bd6b4d258bbf
InkoHX <inkohx@gmail.com>
2019-01-21 19:12:09 +0900

    Fix errors

--- src/VectorNetworkProject/Framework/EliasCore.php
+++ src/VectorNetworkProject/Framework/EliasCore.php
@@ -17,22 +17,15 @@ class EliasCore
     /** @var PluginBase $plugin */
     private static $plugin;
 
-    /** @var MainLogger $logger */
-    private static $logger;
-
     /**
      * @param PluginBase $plugin
      * @param array|null $config
      */
-    public function init(PluginBase $plugin, ?array $config)
+    public static function init(PluginBase $plugin, ?array $config = null)
     {
         self::$plugin = $plugin;
-        self::$logger = new MainLogger(self::getPlugin()->getDataFolder().'elias.log');
         if ($config) {
             Constants::setOption($config);
-        } else {
-            self::$logger->alert('Option is null');
-            self::$plugin->setEnabled(false);
         }
     }
 
@@ -42,13 +35,5 @@ class EliasCore
     public static function getPlugin(): PluginBase
     {
         return self::$plugin;
-    }
-
-    /**
-     * @return MainLogger
-     */
-    public static function getLogger(): MainLogger
-    {
-        return self::$logger;
     }
 }
