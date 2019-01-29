a32b79a7a7a4b30c378d082e99e3e1cfd52d73e7
InkoHX <inkohx@gmail.com>
2019-01-29 02:11:26 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/VectorNetworkProject/Framework/util/Constants.php
+++ src/VectorNetworkProject/Framework/util/Constants.php
@@ -35,7 +35,8 @@ class Constants
 
     /**
      * @param string $key
-     * @param null $return
+     * @param null   $return
+     *
      * @return mixed
      *
      * @example Constants::getOption('provider.user'); // array()
